cc1131b0b4573d6352e639311a69471169140665
Muhammad Hamza Anwar <hamz8191@gmail.com>
2024-01-13 14:53:45 +0500

    Project Guess

---
+++ index.ts
@@ -0,0 +1,48 @@
+import inquirer from "inquirer";
+
+const computerGenNum: number = Math.floor(Math.random()*10);
+let score:number = 0;
+
+async function startloop() {
+    let again;
+    do{
+        await guessNumber();
+    again = await inquirer.prompt([
+        {
+            type: "list",
+            name: "restart",
+            choices: ["Yes", "No"],
+            message: "Do you want to continue "
+        }
+    ])
+}while(again.restart === "Yes");
+}
+    
+startloop();
+
+async function guessNumber() {
+    let guessNum: number = Math.floor(Math.random()*10);
+    let tip: string;
+    if(guessNum % 2==0){
+        tip = "Tip: The number is Even";
+    }
+    else{
+        tip= "Tip: The number is odd";
+    }
+
+    const answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userGuess",
+            message: `Enter a number b/w 1-10: ${tip}`
+        }
+    ]);
+    console.log(`Your Guess: ${answer.userGuess} and system generated number is ${guessNum}`);
+    if(answer.userGuess === guessNum){
+        score++;
+        console.log(`You won, Your score is ${score}`);
+    }
+    else{
+        console.log(`You lose, your score is ${score}`);
+    }
+}
